c335ae7b33973a25b001423153a2b1b9df8fc0ca
Akshat9900 <117174038+Akshat9900@users.noreply.github.com>
2023-02-10 18:20:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload() {
+noseX=0;
+noseY=0;
 
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup() {
@@ -22,13 +25,16 @@ function gotPoses(results)
     if(results.length>0)
     {
         console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x);
-        console.log("nose y ="+ results[0].pose.nose.y);
+        console.log("nose x =" +noseX);
+        console.log("nose y ="+ noseY);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
     }
 }
 
 function draw() {
 image(video,0,0,300,300);
+image(clown_nose,noseX-25,noseY-1,50,30);
 }
 
 function take_snapshot() {
